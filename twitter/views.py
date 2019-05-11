1dda01a978659fb285ffa9ca8008553685b57ee0
mnowotka <mmmnow@gmail.com>
2019-05-11 15:10:24 +0200

    setting up twitter application templates and static files.

--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,10 @@
+from django import views
 from django.shortcuts import render
 
+
 # Create your views here.
+
+class MainWebpageView(views.View):
+    def get(self, request):
+        ctx = {}
+        return render(request, 'twitter/index.html', ctx)
